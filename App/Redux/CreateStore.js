89ff45e39013fd6bd9559a0b642eec63ea94d60d
515074431 <515074431@qq.com>
2018-01-02 13:43:18 +0800

    add redux debugers

--- App/Redux/CreateStore.js
+++ App/Redux/CreateStore.js
@@ -4,6 +4,8 @@ import ReduxPersist from '../Config/ReduxPersist'
 import Config from '../Config/DebugConfig'
 import createSagaMiddleware from 'redux-saga'
 import WebsocketService from '../Services/WebsocketService'
+import devTools from 'remote-redux-devtools'
+import { Platform } from 'react-native'
 // creates the store
 export default (rootReducer, rootSaga) => {
   /* ------------- Redux Configuration ------------- */
@@ -25,7 +27,13 @@ export default (rootReducer, rootSaga) => {
 
   // if Reactotron is enabled (default for __DEV__), we'll create the store through Reactotron
   const createAppropriateStore = Config.useReactotron ? console.tron.createStore : createStore
-  const store = createAppropriateStore(rootReducer, compose(...enhancers))
+  const store = createAppropriateStore(rootReducer, compose(...enhancers,
+                  devTools({
+                    name: Platform.OS,
+                    hostname: 'localhost',
+                    port: 5678
+                  })
+                ))
 
   // configure persistStore and check reducer version number
   if (ReduxPersist.active) {
